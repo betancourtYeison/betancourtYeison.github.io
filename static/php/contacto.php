01509fe53b53e4cbcd31ba01a6f7689711dac027
Yeison Betancourt S <yeisonbe10@hotmail.com>
2015-03-16 09:18:23 -0500

    Update contacto.php

--- static/php/contacto.php
+++ static/php/contacto.php
@@ -1,14 +1,14 @@
 <?php
-	$email_to = "yeisonbe10@hotmail.com";
-	$email_subject = "Contacto desde el sitio web";
+	$email_to = "yeisonbe10@saidw.com";
+	$email_subject = "Contacto desde http://saidw.com/";
 
 	$email_message = "Detalles del formulario de contacto:\n\n";
 	$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
 	$email_message .= "E-mail: " . $_POST['email'] . "\n";
 	$email_message .= "Mensaje: " . $_POST['mensaje'] . "\n\n";
 	
-	$headers = "From: ".$_POST['email']."\r\n".
-	'Reply-To: '.$_POST['email']."\r\n" .
+	$headers = "From: " . $email_to . "\r\n".
+	'Reply-To: ' . $_POST['email'] ."\r\n" .
 	'X-Mailer: PHP/' . phpversion();
 	mail($email_to, $email_subject, $email_message, $headers);
 	
